6c8e94056af42eb65e853d8d7affb798ff38d115
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-07-03 22:37:13 +0300

    Update to Filament its own RichEditor

--- src/LinkAction/Linkpicker.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Codedor\FilamentCustomTiptapExtensions\LinkAction;
-
-use Tiptap\Core\Node;
-
-class Linkpicker extends Node {}
--- tests/Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Codedor\FilamentCustomTiptapExtensions\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
